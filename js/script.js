03d897c06d18c1e3c838c0557ebaedd785c19790
kishimoto-hiyoko <r-kishimoto4271@st.kyoto-art.ac.jp>
2021-11-11 11:12:13 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
 $(function(){
-
   
-})
+    $(".img_button").on("click",function(){
+        $(this).siblings(".content").slideToggle();
+    })
+    
+    $(".close_button").on("click",function(){
+        $(this).parent(".content").slideUp();
+    })
+  
+    let windowH
+    let documentH 
+    let documentW 
+    let scrollTop
+    let separate
+    let scrollRatio
+    
+    $(window).on("scroll resize load",function(){
+      windowH = $(this).height()
+      documentH = $(document).height()
+      documentW = $(document).width()
+      scrollTop = $(this).scrollTop()
+      scrollRatio = scrollTop/(documentH-windowH)
+        
+      separate = documentH / 3;
+    
+      if(scrollTop < separate){
+        $("body").removeClass().addClass("one")
+      }else if(scrollTop < separate * 2){
+        $("body").removeClass().addClass("two")
+      }else{
+        $("body").removeClass().addClass("three")
+      }
+    
+      console.log(windowH,documentH,scrollTop)
+    
+      $("#wave").css("background-position-x",scrollTop/10)
+      $("#line").css("width",scrollRatio * documentW)
+    
+      
+    })
+    $("#btn01").on("click",function(){
+        $("html, body").animate({scrollTop:0}, 1000, "swing");
+      })
+    
+      $("#btn02").on("click",function(){
+        $("html, body").animate({scrollTop:separate*1}, 1000, "swing");
+      })
+    
+      $("#btn03").on("click",function(){
+        $("html, body").animate({scrollTop:separate*2}, 1000, "swing");
+      })
+    
+});    
